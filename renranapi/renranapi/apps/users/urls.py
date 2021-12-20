ba818566142fcb019df6d6e6fedc98dec6256c39
PENGJUNQIAO <1746259155@qq.com>
2021-12-20 06:53:49 -0800

    服务端实现对接防水墙验证码服务器的api接口，校验验证码返回结果

--- renranapi/renranapi/apps/users/urls.py
+++ renranapi/renranapi/apps/users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from rest_framework_jwt.views import obtain_jwt_token
-
+from . import views
 
 urlpatterns = [
     path('login/', obtain_jwt_token),
-
+    path("captcha/", views.CaptchaAPIView.as_view()),
 ]
--- renranapi/renranapi/apps/users/views.py
+++ renranapi/renranapi/apps/users/views.py
@@ -1,3 +1,47 @@
+import json
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.conf import settings
+# urllib python内置的标准模块，用于发送http请求，类似vue里面的axios
+from urllib.parse import urlencode
+from urllib.request import urlopen
+
 
 # Create your views here.
+class CaptchaAPIView(APIView):
+    """验证码"""
+
+    def get(self, request):
+        """接受客户端提交的验证码相关信息"""
+        params = {
+            "aid": settings.TENCENT_CAPTCHA.get("APPID"),
+            "AppSecretKey": settings.TENCENT_CAPTCHA.get("App_Secret_Key"),
+            "Ticket": request.query_params.get("ticket"),
+            "Randstr": request.query_params.get("randstr"),
+            # request._request表示获取django框架的request对象
+            # request._request.META获取本次客户端的请求头
+            "UserIP": request._request.META.get("REMOTE_ADDR")
+        }
+        # 把字典数据转换成地址栏的查询字符串格式
+        # aid=xxx&AppSecretKey=xxx&xxxxx
+        params = urlencode(params)
+        url = settings.TENCENT_CAPTCHA.get("GATEWAY")
+        # 发送http的get请求
+        # urlopen(url=请求地址，data=请求体数据)
+        # 如果get请求，只需要填写参数1
+        # 如果post请求，则需要填写参数1和参数2，其中参数2的格式为字典格式
+        # urlopen是一个资源对象
+        f = urlopen("%s?%s" % (url, params))
+        # https://ssl.captcha.qq.com/ticket/verify?aid=xxx&AppSecretKey=xxx&xxxxx
+
+        # f.read() 读取响应信息
+        content = f.read()
+        res = json.loads(content)
+        if int(res.get("response")) == 1:
+            # 验证成功
+            return Response({"detail": 1})
+        else:
+            # 验证失败
+            return Response({"detail": 0}, status=status.HTTP_400_BAD_REQUEST)
