2b013c66718c674ccaa7a70f776b80f3515a83b4
PENGJUNQIAO <1746259155@qq.com>
2022-01-01 00:08:09 +0800

    基于celery来完成文章的定时发布状态转换

---
+++ renranapi/mycelery/article/tasks.py
@@ -0,0 +1,15 @@
+from mycelery.main import app
+from article.models import Article
+from django.utils import timezone as datetime
+
+
+@app.task(name="interval_pub_article")
+def interval_pub_article():
+    """定时发布"""
+    article_list = Article.objects.filter(pub_date__lte=datetime.now())
+    print(article_list)
+    for article in article_list:
+        article.is_public = True
+        article.pub_date = None
+        article.save()
+        print("文章《%s》发布成功" % article.title)
--- renranapi/mycelery/config.py
+++ renranapi/mycelery/config.py
@@ -1,4 +1,23 @@
+from mycelery.main import app
+from celery.schedules import crontab
+from django.conf import settings
+
 # 任务队列的链接地址
 broker_url = 'redis://127.0.0.1:6379/15'
 # 结果队列的链接地址
 result_backend = 'redis://127.0.0.1:6379/14'
+
+# 和django框架同步时区
+
+app.conf.timezone = settings.TIME_ZONE
+
+app.conf.beat_schedule = {
+    # 定时任务列表
+    'pub-article-every-two-minute': {
+        'task': 'interval_pub_article',  # 指定定时执行的的异步任务
+        # 'schedule': crontab(),            # 时间间隔,一分钟
+        # 'schedule': 120.0,                # 时间间隔,默认:秒
+        'schedule': crontab(minute='*/2'),  # 时间间隔,每2分钟
+        # 'args': (16, 16)                  # 如果任务有固定参数,则可以写在args
+    },
+}
