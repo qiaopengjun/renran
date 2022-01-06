ad4d9a8b4b16e92b5156f5a37d05df8143a8c39d
PENGJUNQIAO <1746259155@qq.com>
2022-01-06 09:58:15 +0800

    前端项目编译完成

--- renran_pc/src/settings.js
+++ renran_pc/src/settings.js
@@ -1,5 +1,6 @@
 export default {
-  Host: "http://api.renran.cn:8000", // ajax请求服务器的地址
+  Host: "http://api.renran.fuguang.top", // ajax请求服务器的地址
+  // Host: "http://api.renran.cn:8000", // ajax请求服务器的地址
   TC_captcha: {
     // 这里的app_id必须和Django中配置里面的app_id一致，否则报错
     app_id: "2060272005",
